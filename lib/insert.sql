07020bb20594d59b6dbb26836037e4173a8da442
CHARITY-WACHIRA <kouwywachira@gmail.com>
2024-06-05 05:01:08 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES(
+'Mr.Chocolate',10,'M', 'Green','calm',1),
+('Rowdy',14, 'M', 'purple','aggresive',1),
+('Tabitha',12,'F','pink','happy',1),
+('Sergeant Brown', 12, 'M', 'brown', 'grumpy', 1),
+('Melissa', 3, 'F', 'tan', 'shy', 1),
+('Grinch', 7, 'M', 'green', 'grumpy', 1),
+('Wendy', 6, 'F', 'brown', 'friendly', 1),
+(NULL, 2, 'M', 'gray', 'curious', 1);
